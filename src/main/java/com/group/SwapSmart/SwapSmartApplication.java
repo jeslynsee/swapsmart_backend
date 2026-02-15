a225d405a8a2df34bf73349da6b196fab151dc32
jeslynsee <jeslynesee@gmail.com>
2026-02-15 15:55:20 -0800

    Initialize Spring Boot project

---
+++ src/main/java/com/group/SwapSmart/SwapSmartApplication.java
@@ -0,0 +1,13 @@
+package com.group.SwapSmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapSmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group/SwapSmart/SwapSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.SwapSmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
